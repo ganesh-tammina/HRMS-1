c19be97080f89925ce9ec2d2f3cd06234caad298
Prakash Babu B <prakash.bandanadham@tammina.org>
2026-03-10 11:21:09 +0530

    New fixes Commit

---
+++ utils/mail.service.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+// Create Transporter (e.g., using Gmail)
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER, // Your email
+    pass: process.env.EMAIL_PASS, // App Password
+  },
+});
+
+// Exported sendMail function
+const sendMail = async (options) => {
+  return await transporter.sendMail({
+    from: '"My App" <' + process.env.EMAIL_USER + '>',
+    ...options,
+  });
+};
+
+module.exports = { sendMail };
