f9face5647dcc2887b69700e4a829954cea7d182
ganesh-tammina <ganesh.meesala@tammina.com>
2026-03-12 11:20:37 +0530

    test commet add dummy text

--- utils/mail.service.js
+++ utils/mail.service.js
@@ -1,20 +1,20 @@
 const nodemailer = require("nodemailer");
 // Create Transporter (e.g., using Gmail)
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER, // Your email
-    pass: process.env.EMAIL_PASS, // App Password
-  },
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER, // Your email
+        pass: process.env.EMAIL_PASS, // App Password
+    },
 });
 
-// Exported sendMail function
+// Exported sendMail function for test email porpuse ganesh
 
 const sendMail = async (options) => {
-  return await transporter.sendMail({
-    from: '"My App" <' + process.env.EMAIL_USER + ">",
-    ...options,
-  });
+    return await transporter.sendMail({
+        from: '"My App" <' + process.env.EMAIL_USER + ">",
+        ...options,
+    });
 };
 
 module.exports = { sendMail };
